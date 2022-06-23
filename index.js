42d5bf7c56e2a5d470e2797a55dd2e430611ef37
Tensor <46503752+TensileTensor@users.noreply.github.com>
2022-06-23 14:36:54 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,8 +2,37 @@
 * Burger Menu
 ****************************************************************************************************/
 
-let burgerIcon = document.getElementById('burger');
-let navbarMenu = document.getElementById('nav-links');
+const burgerIcon = document.getElementById('burger');
+const navbarMenu = document.getElementById('nav-links');
 burgerIcon.addEventListener('click', () => {
     navbarMenu.classList.toggle('is-active');
 })
+
+/****************************************************************************************************
+* Meet the Team (Tabbed Content)
+****************************************************************************************************/
+
+const tabs = document.querySelectorAll('.tabs li');
+const tabContentBoxes = document.querySelectorAll('#tab-content > div');
+
+//Iterate through every tab
+tabs.forEach((tab) => {
+    //Listen for a click on a tab
+    tab.addEventListener('click', () => {
+        //If a click is registered, deactivate all tabs
+        tabs.forEach(item => item.classList.remove('is-active'));
+        //Activate the tab that's been clicked
+        tab.classList.add('is-active');
+        //Swap out content depending on active tag
+        const target = tab.dataset.target;
+
+        tabContentBoxes.forEach(box => {
+            if (box.getAttribute('id') === target) {
+                box.classList.remove('is-hidden');
+            } else {
+                box.classList.add('is-hidden');
+            }
+        })
+
+    });
+});
